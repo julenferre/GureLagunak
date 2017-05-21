3ca0909b04d30b74c91f23957711242caf630922
Julen <julen@gugocreative.com>
2017-05-21 17:21:19 +0200

    Users -> Creado controlador y vista para Users (signup y login)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/signup'
+
+  get 'user/login'
+
   resources :mains
   root 'mains#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
