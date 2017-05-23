e0862083d85e041e2c9f71d73e2d5c351d3cce9b
Julen Ferrero <julenferre@gmail.com>
2017-05-23 19:34:06 +0200

    User atala sortuta. Profile_pic eta pasahitza eguneratu daitezke. Erabiltzailea ezabatu daiteke

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create    
   	@user = User.new(user_params)
     if @user.save 
-      flash.now[:success] = "Ongi etorri GULAG-era!"
+      flash.now[:success] = "Ongi etorri Gure Lagunak-era!"
       redirect_to root_path
       # render html: "BAI :D"
     else
@@ -18,6 +18,52 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def show_photo
+    @user = User.find(params[:id])
+  end
+
+  def show_pass
+    @user = User.find(params[:id])
+  end
+
+  def show_destroy
+    @user = User.find(params[:id])
+  end
+
+  def update_photo
+    @user = User.find(params[:id])
+
+    if @user.update_attribute(:profile_pic, params[:user][:profile_pic])
+      flash.now[:success] = "Argazkia ondo eguneratu da!"
+      redirect_to current_user
+      # render html: "BAI :D"
+    else
+      render 'new'
+      # render html: "Ez :("
+    end
+  end
+
+  def update_password
+    @user = User.find(params[:id])
+
+    if @user.update(params.require(:user).permit(:password, :password_confirmation))
+      flash.now[:success] = "Pasahitza ondo eguneratu da!"
+      redirect_to current_user
+      # render html: "BAI :DD"
+    else
+      render 'new'
+      # render html: "Ez :("
+    end
+  end
+
+  def destroy
+    log_out
+    User.find(params[:id]).destroy
+    flash[:success] = "Erabiltzailea ondo ezabatu da."
+    redirect_to root_url
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,23 @@ Rails.application.routes.draw do
   root 'mains#index'
 
   # Sign Up
-  get '/signup',   to: 'users#new'
-  post '/signup',  to: 'users#create'
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
 
-  #Login/logout
+  # Login/logout
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  # User atala
+  get    '/users/:id',       to: 'users#show'
+  get    '/users/:id/photo', to: 'users#show_photo'
+  patch  '/users/:id/photo', to: 'users#update_photo'
+  get    '/users/:id/pass',  to: 'users#show_pass'
+  patch  '/users/:id/pass',  to: 'users#update_password'
+  get    '/users/:id/destroy', to: 'users#show_destroy'
+  patch  '/users/:id/destroy', to: 'users#destroy'
+
 
   resources :users
 
